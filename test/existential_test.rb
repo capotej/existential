c5e2ddd6abae02fd9b78cc4b16c2ab68622d8eec
Julio Capote <jcapote@gmail.com>
2010-04-22 20:48:43 -0700

    failing test that describes all intended behavior

--- test/existential_test.rb
+++ test/existential_test.rb
@@ -5,14 +5,35 @@ class ExistentialTest < Test::Unit::TestCase
   load_schema
   
   class User < ActiveRecord::Base
+    is_existential
   end
 
   class Post < ActiveRecord::Base
+    def allows_edit_for?(user)
+      if self.user_id == user.id
+        true
+      else
+        false
+      end
+    end
+  end
+  
+  def setup
+    User.destroy_all
+    Post.destroy_all
+    @user = User.create(:name => "fred")
+    @other_guy = User.create(:name => "bob")
+    @post = Post.create(:name => "lolz", :user_id => @user.id)
   end
 
   def test_schema_loaded_correctly
-    assert_equal [], User.all
-    assert_equal [], Post.all
+    assert_equal 2, User.all.length
+    assert_equal 1, Post.all.length
+  end
+
+  def test_post_authorization
+    assert_false @fred.can?(:edit, @post)
+    assert @bob.can?(:edit, @post)
   end
 
 
